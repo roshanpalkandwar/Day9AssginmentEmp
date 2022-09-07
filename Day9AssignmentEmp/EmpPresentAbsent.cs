0144fe603b0102ee2e2bb445373d8e471e1273a2
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-07 15:07:43 +0530

    [roshan]Emp present or absent

---
+++ Day9AssignmentEmp/EmpPresentAbsent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9AssignmentEmp
+{
+    internal class EmpPresentAbsent
+    {
+        public static void EmployeeAttendance()
+        {
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
--- Day9AssignmentEmp/Program.cs
+++ Day9AssignmentEmp/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welecome to employee wages");
+            // Console.WriteLine("welecome to employee wages");
+            Console.WriteLine("chooes number\n1.EMp present absent");
+            int opt=Convert.ToInt32(Console.ReadLine());
+
+            switch(opt)
+            {
+                case 1:
+                    EmpPresentAbsent.EmployeeAttendance();
+                    break;
+            }
         }
     }
 }
