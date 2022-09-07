9b796a3c32573f6624820b65d43b08c8f2d2d473
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-07 14:56:33 +0530

    [roshan]welecome masage to emp

---
+++ Day9AssignmentEmp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day9AssignmentEmp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welecome to employee wages");
+        }
+    }
+}
