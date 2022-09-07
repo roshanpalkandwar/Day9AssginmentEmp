e3bfcf3cac85ff09ecedcb1f08111da36b363d10
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-07 15:13:20 +0530

    [roshan]Daily wages

---
+++ Day9AssignmentEmp/DailyWages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9AssignmentEmp
+{
+    internal class DailyWages
+    {
+        public static void CalculateDailyWage()
+        {
+            int empHrs = 0;
+            const int EMP_RATE_PER_HOUR = 20;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == 0)
+            {
+                Console.WriteLine("Employee is present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                empHrs = 0;
+            }
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage : " + empWage);
+        }
+    }
+}
--- Day9AssignmentEmp/Program.cs
+++ Day9AssignmentEmp/Program.cs
@@ -5,13 +5,16 @@
         static void Main(string[] args)
         {
             // Console.WriteLine("welecome to employee wages");
-            Console.WriteLine("chooes number\n1.EMp present absent");
+            Console.WriteLine("chooes number\n1.EMp present absent\n2.Daily wages");
             int opt=Convert.ToInt32(Console.ReadLine());
 
             switch(opt)
             {
                 case 1:
                     EmpPresentAbsent.EmployeeAttendance();
+                    break;
+                    case 2:
+                    DailyWages.CalculateDailyWage();
                     break;
             }
         }
