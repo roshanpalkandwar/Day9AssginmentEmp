d206882ef154cf325be3fef231dc8c5957218425
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-08 16:10:56 +0530

    [roshan]emp wages for various company

--- Day9AssignmentEmp/Program.cs
+++ Day9AssignmentEmp/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // Console.WriteLine("welecome to employee wages");
-            Console.WriteLine("chooes number\n1.EMp present absent\n2.Daily wages\n3.Part time wages\n4.using switch loop\n5. wages FOr Month\n6.using while loop\n7.Monthly emp wages\n8.wages for compnay");
+            Console.WriteLine("chooes number\n1.EMp present absent\n2.Daily wages\n3.Part time wages\n4.using switch loop\n5. wages FOr Month\n6.using while loop\n7.Monthly emp wages\n8.wages for compnay\n9.emwges for miltiple company");
             int opt=Convert.ToInt32(Console.ReadLine());
 
             switch(opt)
@@ -37,6 +37,20 @@
                     UsingMultipaleCompany.ComputeWaageForMultipleCompany("TCS",20,20,100);  
                     Console.WriteLine();
                     break;
+                case 9:
+
+                    EmpWages TATA = new EmpWages("TATA", 20, 20, 100);
+                    EmpWages BMW = new EmpWages("BMW", 20, 20, 100);
+                    EmpWages TCS = new EmpWages("TCS", 20, 20, 100);
+
+                    TATA.ComputeEmployeeWage();
+                    Console.WriteLine(TATA.ToString());
+                    BMW.ComputeEmployeeWage();
+                    Console.WriteLine(BMW.ToString());
+                     TCS.ComputeEmployeeWage();
+                    Console.WriteLine(TCS.ToString());
+                    break ;
+
             }
         }
     }
